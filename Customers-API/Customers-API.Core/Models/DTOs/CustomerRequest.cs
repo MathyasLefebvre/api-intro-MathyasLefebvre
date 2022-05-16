34aa847a8e2193e4bae030deda7d044c6215dfd9
MathyasLefebvre <mathylefebvre@hotmail.com>
2022-05-16 14:07:42 -0400

    feat - adding the migrations functionality in my project, i migrate can now access to my db

--- Customers-API/Customers-API.Core/Models/DTOs/CustomerRequest.cs
+++ Customers-API/Customers-API.Core/Models/DTOs/CustomerRequest.cs
@@ -3,15 +3,17 @@ namespace Customers_API.Core.Models.DTOs;
 
 public class CustomerRequest
 {
-    [Required]
-    [StringLength(50, MinimumLength = 1)]
-    public string Firstname { get; set; }
+    //[Required] 
+    [Key]
+    public int customer_id { get; set; }
+
+    //[StringLength(50, MinimumLength = 1)]
+    public string firstname { get; set; }
     
-    [Required]
-    [StringLength(50, MinimumLength = 1)]
-    public string Lastname { get; set; }
+    //[StringLength(50, MinimumLength = 1)]
+    public string lastname { get; set; }
     
-    [Required]
-    [EmailAddress]
-    public string Email { get; set; }
+    //[Required]
+    //[EmailAddress]
+    public string email { get; set; }
 }
