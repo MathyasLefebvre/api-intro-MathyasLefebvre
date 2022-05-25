df56cdfa17ec6be2eb0cc045e4afb1b5932048c2
MathyasLefebvre <mathylefebvre@hotmail.com>
2022-05-25 11:27:59 -0400

    feat - creation of interface for the order

--- Customers-API/Customers-API.Core/Interface/IOrderRepository.cs
+++ Customers-API/Customers-API.Core/Interface/IOrderRepository.cs
@@ -1,6 +1,16 @@
+using Customers_API.Core.Models.DTOs;
+
 namespace Customers_API.Core.Interface;
 
-public class IOrderRepository
+public interface IOrderRepository
 {
+    IEnumerable<OrderResponse> GetOrder();
     
+    OrderResponse GetOrderById(int id);
+    
+    void DeleteById(int id);
+    
+    OrderResponse CreateOrder(CreateOrderRequest order);
+    
+    void UpdateOrder(CreateOrderRequest order); 
 }
