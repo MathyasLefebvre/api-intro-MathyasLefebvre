a473bf6ca63ece2d333bc34a1e0746e80563dea4
MathyasLefebvre <mathylefebvre@hotmail.com>
2022-05-25 11:30:05 -0400

    feat - Mapping OrderItem data, Order data, Customer data to the order request

--- Customers-API/Customers-API.Infrastructure/Mapper/OrderMapper.cs
+++ Customers-API/Customers-API.Infrastructure/Mapper/OrderMapper.cs
@@ -6,16 +6,16 @@ namespace Customers_API.Core.Maper;
 public static class OrderMapper
 {
     public static CustomerResponse CustomerToDto(Customer customer)
+    {
+        return new CustomerResponse()
         {
-            return new CustomerResponse()
-            {
-                    CustomerId = customer.CustomerId,
-                    Firstname = customer.Firstname,
-                    Lastname = customer.Lastname,
-                    Email = customer.Email
-            };
-        }
+                Firstname = customer.Firstname,
+                Lastname = customer.Lastname,
+                Email = customer.Email
+        };
+    }
 
+    // va partir
     public static Customer RequestToCustomer(CreateCustomerRequest customer)
     {
         return new Customer
@@ -23,6 +23,54 @@ public static class OrderMapper
                 Firstname = customer.Firstname,
                 Lastname = customer.Lastname,
                 Email = customer.Email
+        };
+    }
+
+    public static OrderResponse OrderToDto(Order order)
+    {
+        return new OrderResponse
+        {
+            CustomerId = order.CustomerId,
+            Country = order.Country,
+            Street = order.Street,
+            City = order.City,
+            ZipCode = order.City,
+            Total = order.Total,
+        };
+    }
+    
+    public static Customer OrderRequestToCustomer(CreateOrderRequest order)
+    {
+        return new Customer
+        {
+                Firstname = order.Customer.Firstname,
+                Lastname = order.Customer.Lastname,
+                Email = order.Customer.Email
+        };
+    }
+
+    public static Order RequestToOrder(CreateOrderRequest request, Customer customer)
+    {
+        return new Order
+        {
+            Country = request.Country,
+            Street = request.Street,
+            City = request.City,
+            ZipCode = request.ZipCode,
+            Total = request.Total,
+            Customer = customer
+        };
+    }
+    
+    public static OrderItem RequestToOrderItem(OrderItemResponse request, Order order)
+    {
+        
+        return new OrderItem
+        {
+           Description = request.Description,
+           Price = request.Price,
+           Quantity = request.Quantity,
+           Order = order
         };
     }
 }
