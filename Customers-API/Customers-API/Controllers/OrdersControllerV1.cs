05c4c5508dd78cedd212aa4c13844a0f4e734bdb
MathyasLefebvre <mathylefebvre@hotmail.com>
2022-05-25 11:31:12 -0400

    feat - Adding the route and the creation of order

--- Customers-API/Customers-API/Controllers/OrdersControllerV1.cs
+++ Customers-API/Customers-API/Controllers/OrdersControllerV1.cs
@@ -1,8 +1,25 @@
+using Customers_API.Core.Interface;
+using Customers_API.Core.Models.DTOs;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Customers_API.Controllers;
 
+[ApiController]
+[Route("api/v1/[controller]")]
 public class OrdersControllerV1: ControllerBase
 {
+    private readonly IOrderRepository _repository;
+
+    public OrdersControllerV1(IOrderRepository repository)
+    {
+        _repository = repository;
+    }
     
+    [HttpPost]
+    public ActionResult<OrderResponse> CreateCustomer(CreateOrderRequest order)
+    {
+        if (!ModelState.IsValid)
+            return NotFound();
+        return Ok(_repository.CreateOrder(order));
+    }
 }
