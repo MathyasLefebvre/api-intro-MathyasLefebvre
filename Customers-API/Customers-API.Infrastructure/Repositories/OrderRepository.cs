493ce51794cde5767520b10f639098eb15cafa7c
MathyasLefebvre <mathylefebvre@hotmail.com>
2022-05-25 11:30:37 -0400

    feat - implementing the IorderRepo and creating new order

--- Customers-API/Customers-API.Infrastructure/Repositories/OrderRepository.cs
+++ Customers-API/Customers-API.Infrastructure/Repositories/OrderRepository.cs
@@ -1,6 +1,76 @@
+using AutoMapper;
+using Customers_API.Core.Interface;
+using Customers_API.Core.Maper;
+using Customers_API.Core.Models.DTOs;
+using Customers_API.Infrastructure.Models;
+
 namespace Customers_API.Infrastructure.Repositories;
 
-public class OrderRepository
+public class OrderRepository: IOrderRepository
 {
-    
+    private DBContext _dbContext;
+
+    public OrderRepository(DBContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    public IEnumerable<OrderResponse> GetOrder()
+    {
+        throw new NotImplementedException();
+    }
+
+    public OrderResponse GetOrderById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public OrderResponse CreateOrder(CreateOrderRequest orderRequest)
+    {
+        var customer = AddCustomer(orderRequest);
+        var order = AddOrder(orderRequest, customer);
+        AddOrderItem(orderRequest, order);
+        var orderResponse = GetOrderResponse(order);
+        return orderResponse;
+    }
+
+    public void UpdateOrder(CreateOrderRequest order)
+    {
+        throw new NotImplementedException();
+    }
+
+    private OrderResponse GetOrderResponse(Order order)
+    {
+        return OrderMapper.OrderToDto(order);
+    }
+
+    private Customer AddCustomer(CreateOrderRequest createOrderRequest)
+    {
+        var customer = OrderMapper.OrderRequestToCustomer(createOrderRequest);
+        _dbContext.Customers.Add(customer);
+        _dbContext.SaveChanges();
+        return customer;
+    }
+
+    private Order AddOrder(CreateOrderRequest createOrderRequest, Customer customer)
+    {
+        var order = OrderMapper.RequestToOrder(createOrderRequest, customer);
+        _dbContext.Orders.Add(order);
+        _dbContext.SaveChanges();
+        return order;
+    }
+
+    private void AddOrderItem(CreateOrderRequest createOrderRequest, Order order)
+    {
+        foreach (var item in createOrderRequest.OrderItems)
+        {
+            var orderItem = OrderMapper.RequestToOrderItem(item, order);
+            _dbContext.OrderItems.Add(orderItem);
+            _dbContext.SaveChanges();
+        }
+    }
 }
