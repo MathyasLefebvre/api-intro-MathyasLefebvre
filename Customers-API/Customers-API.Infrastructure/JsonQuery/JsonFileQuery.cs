e9b54a09beabd7b5d54da63b0b147f3feea3eaed
MathyasLefebvre <mathylefebvre@hotmail.com>
2022-05-25 22:37:54 -0400

    feat - changing ttype of method to public so i can use them later

--- Customers-API/Customers-API.Infrastructure/JsonQuery/JsonFileQuery.cs
+++ Customers-API/Customers-API.Infrastructure/JsonQuery/JsonFileQuery.cs
@@ -3,48 +3,46 @@ using Customers_API.Infrastructure.Models;
 
 namespace Customers_API.Infrastructure;
 
-public static class Query
+public static class JsonFileQuery
 {
-    public static string GetCountryData(DBContext context)
+    public static Country GetCountryData(DBContext context)
     {
-        var country = new Country
+        return new Country
         {
                 Canada = GetCanadaData(context),
                 Other = GetOtherCountryData(context)
         };
-        return JsonSerializer.Serialize(country);
     }
     
-    public static string GetOrdersData(DBContext context)
+    public static NumberOrders GetOrdersData(DBContext context)
     {
-        var orders =  new NumberOrders
+        return new NumberOrders
         {
                 NumberOrder = GetNumberOfOrders(context),
                 NumberOrderItem = GetNumberOfOrdersItems(context)
         };
-        return JsonSerializer.Serialize(orders);
     }
 
-    private static int GetCanadaData(DBContext context)
+    public static int GetCanadaData(DBContext context)
     {
         return context.Orders.Where(o => o.Country == "Canada")
                 .Select(o => o.Country)
                 .Count();
     }
     
-    private static int GetOtherCountryData(DBContext context)
+    public static int GetOtherCountryData(DBContext context)
     {
         return context.Orders.Where(o => o.Country != "Canada")
                 .Select(o => o.Country)
                 .Count();
     }
 
-    private static int GetNumberOfOrders(DBContext context)
+    public static int GetNumberOfOrders(DBContext context)
     {
         return context.Orders.Select(o => o.OrderId).Count();
     }
     
-    private static int GetNumberOfOrdersItems(DBContext context)
+    public static int GetNumberOfOrdersItems(DBContext context)
     {
         return context.OrderItems.Select(o => o.OrderItemId).Count();
     }
